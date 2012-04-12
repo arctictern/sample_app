637838e5e5e89d7e65041d97cbd1abe430c9bb92
Cherie Guo <guocherry@gmail.com>
2012-04-12 17:19:10 -0500

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+       flash[:success] = "welcome to the sample application"
+       redirect_to @user    
+    else
+      render 'new'
+    end
+  end
+
   def new
+    @user = User.new
   end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name  "cherie"
+    email "guoguo@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
