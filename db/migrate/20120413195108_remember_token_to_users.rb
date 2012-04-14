f96a7e4dd7a5ea37534ba14783ba6a2eba68d448
Cherie Guo <guocherry@gmail.com>
2012-04-13 22:25:05 -0500

    Finish sign in

---
+++ db/migrate/20120413195108_remember_token_to_users.rb
@@ -0,0 +1,7 @@
+class RememberTokenToUsers < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
---
+++ db/migrate/20120413195305_add_token_to_users.rb~
@@ -0,0 +1,4 @@
+class AddTokenToUsers < ActiveRecord::Migration
+  def change
+  end
+end
