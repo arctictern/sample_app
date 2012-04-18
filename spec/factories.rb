a3a27d25360c4fe0e9097765f486e29b3e808e2f
Cherie Guo <guocherry@gmail.com>
2012-04-18 12:41:36 -0500

    Finish user edit, update, destroy

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
   factory :user do
-    name  "cherie"
-    email "guoguo@gmail.com"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email){ |n| "person_#{n}@example.com" }
     password "foobar"
     password_confirmation "foobar"
+    
+    factory :admin do
+      admin true
+    end
+
+
   end
 end
